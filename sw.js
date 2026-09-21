164004dca257061f967deb60d39a72fb7894bb18
wayanrosyid-oss <wayanrosyid@gmail.com>
2026-09-21 13:52:21 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker untuk Belanja Logistik Trip Cito */
-const CACHE_NAME = 'logistik-trip-cito-v2';
+const CACHE_NAME = 'logistik-trip-cito-v4';
 
 const APP_SHELL = [
   './index.html',
